d2118c0b37a4a1117582926b43831dcfe644aac6
EA1GIY <ea1giy@gmail.com>
2020-09-21 10:02:18 +0200

    Basic template

--- src/emergencybot.py
+++ src/emergencybot.py
@@ -11,6 +11,27 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="This is just a test")
 
 
+def emergencia():
+    # Notificar emergencia a personal de guardia
+    # Solo puede ser activado por los administradores del grupo
+    pass
+
+
+def cuadrante():
+    # Menu para la modificación y consulta del cuadrante
+    pass
+
+
+def modificarCuadrante():
+    # Sección para modificar los horarios del cuadrante.
+    pass
+
+
+def consultarCuadrante():
+    # Consulta o descarga de cuadrante actual
+    pass
+
+
 # Registro de los diferentes comandos del bot
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
@@ -18,4 +39,5 @@ dispatcher.add_handler(start_handler)
 
 # Comenzar actualizaciones del bot
 updater.start_polling()
+updater.idle()
 
