aa7da1d2a4588ccadb068a2d37a5d222206430ae
EA1GIY <ea1giy@gmail.com>
2020-09-22 22:36:46 +0200

    Minor corrections

--- src/emergencybot.py
+++ src/emergencybot.py
@@ -8,33 +8,37 @@ def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="This is just a test")
 
 
-def emergencia():
+def emergencia(update, context):
     # Notificar emergencia a personal de guardia
     # Solo puede ser activado por los administradores del grupo
-    pass
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Emergencia")
 
 
-def cuadrante():
+def cuadrante(update, context):
     # Menu para la modificación y consulta del cuadrante
-    pass
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Cuadrante")
 
 
-def modificar_cuadrante():
+def modificar_cuadrante(update, context):
     # Sección para modificar los horarios del cuadrante.
-    pass
+    context.bot.send_message(chat_id=update.effective_chat.id, text="modificar cuadrante")
 
 
-def consultar_cuadrante():
+def consultar_cuadrante(update, context):
     # Consulta o descarga de cuadrante actual
-    pass
+    context.bot.send_message(chat_id=update.effective_chat.id, text="consultar cuadrante")
 
 
 def main():
-    updater = Updater(token='TOKEN', use_context=True)
+    updater = Updater(token='token', use_context=True)
     dp = updater.dispatcher
 
     # Registro de los diferentes comandos del bot.
     dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('emergencia', emergencia))
+    dp.add_handler(CommandHandler('cuadrante', cuadrante))
+    dp.add_handler(CommandHandler('Modificar', modificar_cuadrante))
+    dp.add_handler(CommandHandler('Consultar', consultar_cuadrante))
 
     # Iniciar bot en modo temporizado
     updater.start_polling()
